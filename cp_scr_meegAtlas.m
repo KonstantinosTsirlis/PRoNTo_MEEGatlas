ed5ef548abace2c83d94f47b2cffd76a5584ecea
Christophe Phillips <c.phillips@uliege.be>
2020-04-10 16:30:33 +0200

    Fixing path after moving data into subfolder

--- cp_scr_meegAtlas.m
+++ cp_scr_meegAtlas.m
@@ -4,7 +4,7 @@
 pth_root = 'C:\Dox\1_Code\PRoNTo_Hackathon2020';
 
 % define the input parameters for 1chan/2D case
-fn_data = fullfile(pth_root,'condition_Famous.nii');
+fn_data = fullfile(pth_root,'2Ddata','condition_Famous.nii');
 Vin = spm_vol(fn_data);
 blocks_def = struct( ...
     'chans', 1, ...
@@ -14,7 +14,7 @@ labels_def.chans = {'chanXYZ'};
 labels_def.twind = cellstr(char('early','medium','late'));
 labels_def.fband =  cellstr(char('beta','alpha')) ; 
 dorder = 'ftc'; % order of axis in current data: frequency, time, channels
-fn_out = fullfile(pth_root,'test_MEEGatlas.nii');
+fn_out = fullfile(pth_root,'test2D_MEEGatlas.nii');
 % call to prt_create_meegAtlas
 [Vout,fn_labels] = prt_create_meegAtlas(Vin,fn_out,blocks_def,labels_def,dorder);
 
@@ -30,7 +30,7 @@ S.mode = 'time x frequency';
 
 
 % define the input parameters for N_chan/3D case
-fn_data = fullfile(pth_root,'rtf_EEG_mapMcbdspmeeg_run_01_sss', ...
+fn_data = fullfile(pth_root,'3Ddata','rtf_EEG_mapMcbdspmeeg_run_01_sss', ...
     'condition_Famous.nii');
 Vin = spm_vol(fn_data);
 blocks_def = struct( ...
